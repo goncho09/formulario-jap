a313d51d372f554db78e2530c53772b422b6e537
Donovan Acosta <dnvnacosta81@gmail.com>
2025-08-13 13:33:23 -0300

    confirmar terminos

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,22 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const check = document.getElementById('terminos');
+const button = document.getElementById('regBtn');
+
+button.addEventListener('click', function () {
+    
+    checkBoxIsChecked();
+
+})
+
+
+function checkBoxIsChecked() {
+   if(!check.checked) {
+        showAlertError();
+        return;
+    }
+
+    showAlertSuccess();
+}
